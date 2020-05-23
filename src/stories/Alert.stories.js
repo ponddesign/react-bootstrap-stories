83763a09fb9f97c20d7f318c48a8d88b9abd6cbd
Christopher Pond <christopher.pond@endurance.com>
2020-05-23 11:47:46 -0500

    chore: initial sb x cra setup

---
+++ src/stories/Alert.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Alert } from "react-bootstrap";
+
+export default {
+  title: "Components",
+  component: Alert,
+};
+
+export const alert = () => <Alert>Some warning text</Alert>;
